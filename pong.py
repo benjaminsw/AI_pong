0b9fb499c88356b8e9776dfa2fb2d2b4d0fe7d59
benjaminsw <benjaminsw@live.com>
2017-01-22 01:34:10 +0700

    create PongGame class

--- pong.py
+++ pong.py
@@ -38,8 +38,96 @@ def drawBall(ballXPos, ballYPos):
     pygame.draw.rect(screen, WHITE, ball)
 
 #draw paddle
-def drawPaddle(PaddleXPos):
+def drawPaddle(paddleXPos, paddleYPos):
     #create a bar for paddle
-    paddle = pygame.Rect()
+    paddle = pygame.Rect(paddleXPos, paddleYPos, PADDLE_WIDTH, PADDLE_HEIGHT)
+    pygame.draw.rect(screen, WHITE, paddle)
 
+# update the ball, using the paddle positions, teh ball positions and the balls direction
+def updateBall(paddleXPos, ballXPos, ballYPos, ballXDirection, ballYDirection):
+
+    #update x and y position for ball
+    ballXPos = ballXPos + ballXDirection * BALL_X_SPEED
+    ballYPos = ballYPos + ballYDirection * BALL_Y_SPEED
+
+    #check for the collistion on the paddle
+    if(ballXPos <= PADDLE_BUFFER + PADDLE_WIDTH and ballXPos + BALL_HEIGHT >= paddleXPos and ballXPos + BALL_HEIGHT <= paddleXPos + PADDLE_HEIGHT):
+        #switch the direction
+        ballXDirection = 1
+    elif (ballXDirection <= 0):
+        #negative score
+        ballXDirection = 1
+        score = -1
+        return [score, paddleXPos, ballXPos, ballYPos, ballXDirection, ballYDirection]
+
+    #if it hits the top, then move down
+    if (ballYPos <= 0):
+        ballYPos = 0;
+        ballYDirection = 1;
+    #if it hits the bottom, then move up
+    elif (ballYPos >= WINDOW_HEIGHT - BALL_HEIGHT):
+        ballYPos = WINDOW_HEIGHT - BALL_HEIGHT
+        ballYDirection = -1
+    return [score, paddleYPos, ballXPos, ballYPos, ballXDirection, ballYDirection]
+
+    #update the paddle position
+
+def updatePaddle(action, paddleXPos):
+    #if move up
+    if(action[1] == 1):
+        paddleYPos = paddleXPos - PADDLE_SPEED
+    #if move down
+    if (action[2] == 1):
+        paddleYPos = paddleXPos + PADDLE_SPEED
+
+    # don't let move out of the screen
+    if(paddleXPos < 0):
+        paddleXPos = 0
+    if(paddleXPos > WINDOW_HEIGHT - PADDLE_HEIGHT):
+        paddleXPos = WINDOW_HEIGHT - PADDLE_HEIGHT
+    return paddleYPos
+
+#game class
+class PongGame:
+    def __init__(self):
+        #random number for initial direction of ball
+        #keep score
+        self.tally = 0
+        #initialie position of paddle
+        self.paddleYPos = WINDOW_HEIGHT / 2 -PADDLE_HEIGHT /2
+        #ball direction
+        self.ballXDirection = 1
+        self.ballYDirection = 1
+        #restarting point
+        self.ballXPos = WINDOW_WIDTH / 2 - BALL_WIDTH / 2
+
+        #randomly decide where the ball will move
+        if(0 < num < 3 ):
+            self.ballXDirection = 1
+            self.ballYDirection = 1
+        if(3 <= num < 5):
+            self.ballXDirection = -1
+            self.ballYDirection = 1
+        if(5 <= num < 8):
+            self.ballXDirection = 1
+            self.ballYDirection = -1
+        if(8 <= num < 10):
+            self.ballXDirection = -1
+            self.ballYDirection = -1
+
+        #new random number
+        num = random.randInt(0,9)
+        #where it will start, y part
+        self.ballYPos = num * (WINDOW_HEIGHT - BALL_HEIGHT)/9
+
+    def getPresentFrame(self):
+        #for each frame, calls the event queue, like if the main window needs to be repainted
+        pygame.event.pump()
+        #make the background back
+        screen.fill(BLACK)
+        #draw our paddles
+        drawPaddle(self.paddleYPos)
+        #draw the ball
+        drawBall(self.ballXPos, self.ballYPos)
+        #copy the pixel from our surface to a 3D array. we'll use this for RL
 
